cbf5bbcba31db3ea45ace4207e113732daa07764
Eric Cezimbra <eric.cezimbra@gmail.com>
2026-05-27 14:10:26 -0300

    feat: Adicionando Schemas

---
+++ ws-work-backend/schemas.py
@@ -0,0 +1,54 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+# Schemas para a marca
+
+class MarcaBase(BaseModel):
+    nome_marca: str
+
+class MarcaCreate(MarcaBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+# Schemas para modelo
+
+class ModeloBase(BaseModel):
+    marca_id: int
+    nome: str
+    valor_fipe: float
+
+class ModeloCreate(ModeloBase):
+    pass
+
+class ModelResponse(ModeloBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+# Schemas para o carro
+
+class CarroBase(BaseModel):
+    modelo_id: int
+    ano: int
+    combustivel: str
+    num_portas: int
+    cor: str
+    quilometragem: int
+    valor_anuncio: float
+    descricao: str
+
+class CarroCreate(CarroBase):
+    pass
+
+class CarroResponse(CarroBase):
+    id: int 
+    timestamp_cadastro: datetime
+
+    nome_modelo: str | None = None
+    nome_marca: str | None = None
+
+    class Config:
+        from_attributes = True
